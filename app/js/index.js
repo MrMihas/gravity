1ea48eaaaece3ae1bcfdc29414d770fe7dd5d5f4
Mihas <32958803+MrMihas@users.noreply.github.com>
2020-03-28 20:01:57 +0200

    Update index.js

--- app/js/index.js
+++ app/js/index.js
@@ -1,21 +1,21 @@
 //эффект для кнопки
 
-const buttons = document.querySelectorAll('.hero__button a');
+// const buttons = document.querySelectorAll('.hero__button a');
 
-buttons.forEach(function(btn){
-btn.addEventListener("click", function(e){
+// buttons.forEach(function(btn){
+// btn.addEventListener("click", function(e){
 
-	let x = e.clientX - e.target.offsetLeft;
-	let y = e.clientY - e.target.offsetTop;
+// 	let x = e.clientX - e.target.offsetLeft;
+// 	let y = e.clientY - e.target.offsetTop;
 
-	let ripples = document.createElement('span');
+// 	let ripples = document.createElement('span');
 
-	ripples.style.left = x + 'px';
-	ripples.style.top = y + 'px';
-	this.appendChild(ripples);
-	setTimeout(function(){
-		ripples.remove();
-	}, 1000);
+// 	ripples.style.left = x + 'px';
+// 	ripples.style.top = y + 'px';
+// 	this.appendChild(ripples);
+// 	setTimeout(function(){
+// 		ripples.remove();
+// 	}, 1000);
 
-})
-})
+// })
+// })
