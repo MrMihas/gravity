abe6c7401190378adcb00024cfb5b3f4f91edd8e
Michail Gancherenko <m.gancherenko@nton.info>
2020-03-29 13:40:02 +0300

    add menu

--- app/js/index.js
+++ app/js/index.js
@@ -1,21 +1,53 @@
 //эффект для кнопки
 
-const buttons = document.querySelectorAll('.hero__button a');
+// const buttons = document.querySelectorAll('.hero__button a');
 
-buttons.forEach(function(btn){
-btn.addEventListener("click", function(e){
+// buttons.forEach(function(btn){
+// btn.addEventListener("click", function(e){
 
-	let x = e.clientX - e.target.offsetLeft;
-	let y = e.clientY - e.target.offsetTop;
+// 	let x = e.clientX - e.target.offsetLeft;
+// 	let y = e.clientY - e.target.offsetTop;
 
-	let ripples = document.createElement('span');
+// 	let ripples = document.createElement('span');
 
-	ripples.style.left = x + 'px';
-	ripples.style.top = y + 'px';
-	this.appendChild(ripples);
-	setTimeout(function(){
-		ripples.remove();
-	}, 1000);
+// 	ripples.style.left = x + 'px';
+// 	ripples.style.top = y + 'px';
+// 	this.appendChild(ripples);
+// 	setTimeout(function(){
+// 		ripples.remove();
+// 	}, 1000);
 
-})
-})
+// })
+// })
+
+
+// раздел меню
+
+// open and clz menu
+const menuLines = document.querySelector(".lines");
+const headerFixed = document.querySelector("header");
+menuLines.onclick = function(e){
+	e.preventDefault();
+	const menu = document.querySelector(".menu__container");
+
+	this.classList.toggle("close");
+	menu.classList.toggle("open");
+	headerFixed.classList.toggle("fixed");
+
+}
+
+
+
+
+let navLink   = document.querySelectorAll(".nav-link");
+
+for (let i = 0; i < navLink.length; i++) {
+	navLink[i].onclick = function(){
+		const menu = document.querySelector(".menu__container");
+		const menuLines = document.querySelector(".lines");
+const headerFixed = document.querySelector("header");
+		menu.classList.remove("open");
+		menuLines.classList.remove("close");
+		headerFixed.classList.remove("fixed");
+	}
+}
