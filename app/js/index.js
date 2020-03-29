45e91611412bc65df350b4ebce3360c4c9929fb4
Mihas <32958803+MrMihas@users.noreply.github.com>
2020-03-29 14:00:59 +0300

    add menu (#1)
    Co-authored-by: Michail Gancherenko <m.gancherenko@nton.info>

--- app/js/index.js
+++ app/js/index.js
@@ -19,3 +19,38 @@
 
 // })
 // })
+
+
+
+// раздел меню
+
+// open and clz menu
+const menuLines = document.querySelector(".lines");
+const headerFixed = document.querySelector("header");
+menuLines.onclick = function(e){
+	e.preventDefault();
+	const menu = document.querySelector(".menu__container");
+
+	this.classList.toggle("close");
+	menu.classList.toggle("open");
+	headerFixed.classList.toggle("fixed");
+
+}
+
+
+
+
+let navLink   = document.querySelectorAll(".nav-link");
+
+for (let i = 0; i < navLink.length; i++) {
+	navLink[i].onclick = function(){
+		const menu = document.querySelector(".menu__container");
+		const menuLines = document.querySelector(".lines");
+const headerFixed = document.querySelector("header");
+		menu.classList.remove("open");
+		menuLines.classList.remove("close");
+		headerFixed.classList.remove("fixed");
+	}
+}
+
+
